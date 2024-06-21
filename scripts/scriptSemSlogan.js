90050c3ab236b98a9555c0a529d46cf6386f398a
Gustavo da Costa <gustavocosta.nh@gmail.com>
2024-06-21 16:18:47 -0300

    done until find us

--- scripts/scriptSemSlogan.js
+++ scripts/scriptSemSlogan.js
@@ -1,15 +1,3 @@
-document.getElementById('slogan').onclick = function() {
-
-	var url = 'https://www.instagram.com/cabanhasaonunca/';
-
-
-	var novaGuia = window.open(url, '_blank');
-
-	if (novaGuia) {
-		novaGuia.focus();
-	}
-};
-
 document.getElementById('navbar__nosEncontre').onclick = function() {
 	window.location.href = "../place.html";
 };
