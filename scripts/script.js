87b421a871d8ca7c46461de996a8f748d9e76550
Gustavo da Costa <gustavocosta.nh@gmail.com>
2024-06-21 16:41:18 -0300

    corrected dynamic reference

--- scripts/script.js
+++ scripts/script.js
@@ -10,18 +10,22 @@ document.getElementById('slogan').onclick = function() {
 	}
 };
 
+// const basePath = window.location;
+
+let fullUrl = window.location.href.replace('index.html', '').replace('breeds.html', '').replace('contactform.html', '').replace('place.html', '');
+
 document.getElementById('navbar__nosEncontre').onclick = function() {
-	window.location.href = "../place.html";
+	window.location.href = fullUrl + "/place.html";
 };
 
 document.getElementById('navbar__racas').onclick = function() {
-	window.location.href = "../breeds.html";
+	window.location.href = fullUrl + "/breeds.html";
 };
 
 document.getElementById('navbar__contato').onclick = function() {
-	window.location.href = "../contactform.html";
+	window.location.href = fullUrl + "/contactform.html";
 };
 
 document.getElementById('navbar__home').onclick = function() {
-	window.location.href = "../index.html";
+	window.location.href = fullUrl + "/index.html";
 };
