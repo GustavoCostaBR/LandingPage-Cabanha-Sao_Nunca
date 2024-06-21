e705495b1855a7f45f4ebb617feaaa5e6090c78a
Gustavo da Costa <gustavocosta.nh@gmail.com>
2024-06-21 17:01:58 -0300

    corrected dynamic adreess

--- scripts/script.js
+++ scripts/script.js
@@ -12,20 +12,53 @@ document.getElementById('slogan').onclick = function() {
 
 // const basePath = window.location;
 
-let fullUrl = window.location.href.replace('index.html', '').replace('breeds.html', '').replace('contactform.html', '').replace('place.html', '');
+let fullUrl = window.location.href;
+let verifier = 0;
+
+console.log(fullUrl);
+
+if (fullUrl.includes("github")) {
+	fullUrl = fullUrl.replace('/index', '').replace('/breeds', '').replace('/contactform', '').replace('/place', '');
+	verifier = 1;
+}
+
+if (fullUrl.includes("localhost") || fullUrl.includes("127.0.0.1")) {
+	fullUrl = fullUrl.replace('index.html', '').replace('breeds.html', '').replace('contactform.html', '').replace('place.html', '');
+}
 
 document.getElementById('navbar__nosEncontre').onclick = function() {
-	window.location.href = fullUrl + "/place.html";
+	if (verifier == 1) {
+		window.location.href = fullUrl + "place";
+	}
+	else {
+		window.location.href = fullUrl + "place.html";
+	}
+
 };
 
 document.getElementById('navbar__racas').onclick = function() {
-	window.location.href = fullUrl + "/breeds.html";
+	if (verifier == 1) {
+		window.location.href = fullUrl + "breeds";
+	}
+	else {
+		window.location.href = fullUrl + "breeds.html";
+	}
 };
 
 document.getElementById('navbar__contato').onclick = function() {
-	window.location.href = fullUrl + "/contactform.html";
+	if (verifier == 1) {
+		window.location.href = fullUrl + "contactform";
+	}
+	else {
+		window.location.href = fullUrl + "contactform.html";
+	}
 };
 
 document.getElementById('navbar__home').onclick = function() {
-	window.location.href = fullUrl + "/index.html";
+	if (verifier == 1) {
+		window.location.href = fullUrl + "index";
+	}
+	else {
+		window.location.href = fullUrl + "index.html";
+	}
 };
