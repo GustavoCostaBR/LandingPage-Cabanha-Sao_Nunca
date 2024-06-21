5940c04becf1369b0cb2d13951dd6370b20e25a3
Gustavo da Costa <gustavocosta.nh@gmail.com>
2024-06-21 15:07:28 -0300

    working on navbar

--- scripts/script.js
+++ scripts/script.js
@@ -9,3 +9,15 @@ document.getElementById('slogan').onclick = function() {
 		novaGuia.focus();
 	}
 };
+
+document.getElementById('navbar__nosEncontre').onclick = function() {
+	window.location.href = "../place.html";
+};
+
+document.getElementById('navbar__racas').onclick = function() {
+	window.location.href = "../breeds.html";
+};
+
+document.getElementById('navbar__contato').onclick = function() {
+	window.location.href = "../contactform.html";
+};
